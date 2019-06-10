fdc9cc5fdfde42340f7bd768aa27e536829df793
wangch <wangchunhui@51-dg.com>
2019-06-10 15:34:15 +0800

    init laravel-tool

---
+++ src/Library/Upload/Upload.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 86176
+ * Date: 2019/6/10
+ * Time: 15:29
+ */
+
+namespace Common;
+
+class Upload
+{
+    public function index(){
+        echo 1;
+    }
+}
